0d72f930b23ef532f946f8bb3c0a85264c7ca6fe
anthonygrees <anthony_rees01@hotmail.com>
2018-05-11 14:46:22 +1000

    fix client.rd

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,25 +3,31 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-bash 'extract_module' do
+bash 'Do some chef pre-work' do
     code <<-EOH
 
 #!/bin/bash -xev
-
-# Do some chef pre-work
 /bin/mkdir -p /etc/chef
 /bin/mkdir -p /var/lib/chef
 /bin/mkdir -p /var/log/chef
 
-# Setup hosts file correctly
+EOH
+end
+
+bash 'Setup hosts file correctly' do
+    code <<-EOH
 cat > "/etc/hosts" << EOF
 34.216.239.205 chef.automate-demo.com
 34.211.141.147 automate.automate-demo.com
 EOF
 
+EOH
+end
+
+bash 'Install chef' do
+    code <<-EOH
 cd /etc/chef/
 
-# Install chef
 curl -L https://omnitruck.chef.io/install.sh | bash || error_exit 'could not install chef'
 
 # Create first-boot.json
@@ -33,14 +39,25 @@ cat > "/etc/chef/first-boot.json" << EOF
 }
 EOF
 
+EOH
+end
+
+bash 'Create client.rb' do
+    code <<-EOH
+
 NODE_NAME=node-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
 
-# Create client.rb
 /bin/echo 'log_location     STDOUT' >> /etc/chef/client.rb
-/bin/echo -e "chef_server_url  \"https://chef.automate-demo.com/organizations/automate\"" >> /etc/chef/client.rb
-/bin/echo -e "validation_client_name \"automate-validator\"" >> /etc/chef/client.rb
-/bin/echo -e "validation_key \"/etc/chef/validator.pem\"" >> /etc/chef/client.rb
-/bin/echo -e "node_name  \"${NODE_NAME}\"" >> /etc/chef/client.rb
+/bin/echo -e "chef_server_url 'https://chef.automate-demo.com/organizations/automate' >> /etc/chef/client.rb
+/bin/echo -e "validation_client_name 'automate-validator' >> /etc/chef/client.rb
+/bin/echo -e "validation_key '/tmp/kitchen/cookbooks/bjc_linux_bootstrap/recipes/validator.pem' >> /etc/chef/client.rb
+/bin/echo -e "node_name '${NODE_NAME}' >> /etc/chef/client.rb
+/bin/echo -e "ssl_verify_mode :verify_none" >> /etc/chef/client.rb
+EOH
+end
+
+bash 'Run It' do
+    code <<-EOH
 
 sudo chef-client -j /etc/chef/first-boot.json
 
