4b6957c12e95f17a8a9e98083a62a5aa5634d56b
Anthony Rees <anthony_rees01@hotmail.com>
2020-04-07 11:13:18 +1000

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,7 +20,7 @@ bash 'Create client.rb' do
 NODE_NAME=CentOS-AR-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
 
 /bin/echo 'log_location     STDOUT' >> /etc/chef/client.rb
-/bin/echo -e "chef_server_url 'https://#{ENV['AUTOMATE_HOSTNAME']}/organizations/#{ENV['CHEF_ORG']}'" >> /etc/chef/client.rb
+/bin/echo -e "chef_server_url 'https://#{default['environment']['automate_url']}/organizations/#{default['environment']['chef_org']}'" >> /etc/chef/client.rb
 /bin/echo -e "validation_key '/tmp/kitchen/cookbooks/linux_node/recipes/validator.pem'" >> /etc/chef/client.rb
 /bin/echo -e "node_name '${NODE_NAME}'" >> /etc/chef/client.rb
 /bin/echo -e "ssl_verify_mode :verify_none" >> /etc/chef/client.rb
